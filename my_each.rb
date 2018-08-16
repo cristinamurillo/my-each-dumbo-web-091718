55d7ae5b53ed613145b93db93343ca543cc12c8a
Cristina Murillo <cmurill1@jhu.edu>
2018-08-16 16:27:52 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  new_array = []
+  if block_given?
+    i = 0 
+    while i < array.length 
+      yield array[i]
+      new_array << array[i]
+      i+=1 
+    end
+  end
+  new_array 
 end
